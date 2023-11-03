69168b16c5fe49af05d5a94aeedf4b7f692637c4
TIA-AHMED <tia.ahmed@hotmail.com>
2023-11-04 00:48:21 +0500

    req done

--- template.py
+++ template.py
@@ -9,7 +9,7 @@ list_of_files=[
     f"src/{project_name}/__init__.py",
     f"src/{project_name}/conponents/__init__.py",
     f"src/{project_name}utils/common.py",
-    f"src/{project_name}logging/__init__.py",
+    f"src/{project_name}/logging/__init__.py",
     f"src/{project_name}/config/__init__.py",
     f"src/{project_name}/pipeline/__init__.py",
     f"src/{project_name}/entity/__init__.py",
